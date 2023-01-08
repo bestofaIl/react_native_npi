e44b2b048ae1e1e4b4e622ea16f35d8097c62f82
Артём Абрамян <abramyan.ar.ar@gmail.com>
2023-01-08 22:13:44 +0300

    first

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [date, setDate] = useState("Текущая дата");
+
+  const updateDate = () => {
+    setDate(new Date().toDateString());
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{date}</Text>
+      <Button title="Get date!" onPress={updateDate}></Button>
     </View>
   );
 }
